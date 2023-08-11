79acfad2d5cd07c9cb44878069245a8723308935
Abdulhaiqinaat <124757812+Abdulhaiqinaat@users.noreply.github.com>
2023-08-12 03:20:05 +0500

    Add files via upload
    Your parents will let you go to the beach with your friends if you have written 1500 or more
    words for your term project. Write a program that asks the user how many words they have
    written and then prints out one of the two following messages, based on the resul.
    Great, you've done your homework. Enjoy the beach!
    Sorry, you must stay home and work on your term paper.

---
+++ project_If_else.py
@@ -0,0 +1,10 @@
+'''Your parents will let you go to the beach with your friends if you have written 1500 or more
+words for your term project. Write a program that asks the user how many words they have
+written and then prints out one of the two following messages, based on the resul.
+Great, you've done your homework. Enjoy the beach!
+Sorry, you must stay home and work on your term paper.'''
+x=int(input("How many words you have written= "))
+if x==1500:
+         print("Great you have done your homework,Enjoy the beatch !!!")
+else:
+     print("Sorry, you must stay home and work on your term paper,")
